d76e65d369137ef74e8eb5f761a28fd9f5fb3e84
alphatra <alpphatra@gmail.com>
2022-12-31 20:54:20 +0100

    css template v.1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useReducer } from 'react'
 import './App.css'
 
+
 function App() {
+
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='Calculator'>
+      <div className='Display'>
+        <div className='previous-output'>{previousOperand} {operation}</div>
+        <div className='current-output'>{currentOperand}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='Keypad'>
+        <div className='left-panel'>
+          <button>sin</button>
+          <button>ln</button>
+          <button>cos</button>
+          <button>log</button>
+          <button>tan</button>
+          <button>e</button>
+          <button>pi</button>
+          <button>pow</button>
+          <button>!</button>
+          <button>sqrt</button>
+        </div>
+
+        <div className='main-panel'>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>0</button>
+          <button>.</button>
+          <button>DEL</button>
+        </div>
+
+        <div className='right-panel'>
+          <button>÷</button>
+          <button>(</button>
+          <button>×</button>
+          <button>)</button>
+          <button>-</button>
+          <button>=</button>
+          <button>+</button>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
